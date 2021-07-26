d3abb841e6eea35ea89ddb830bb24d7a2514fb26
Michael Randall <m_ran66@hotmail.com>
2021-07-26 07:54:26 -0700

    added error handling to putDevice

--- putDevice/index.js
+++ putDevice/index.js
@@ -5,8 +5,6 @@ const { CosmosClient } = require("@azure/cosmos");
 const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
-// const databaseId = "dvc-mngmnt-db";
-// const containerId = "devices";
 const databaseId = config.databaseId;
 const containerId = config.devicesContainerId;
 
@@ -20,7 +18,6 @@ const secretClient = new SecretClient(keyVaultUri, credential);
 
 module.exports = async function (context, req) {
   const endpoint = config.endpoint;
-  // const secretKey = await secretClient.getSecret("dvcnamingCosmosPKey");
   const secretKey = await secretClient.getSecret(config.keyvaultkey);
   const key = secretKey.value;
   const client = new CosmosClient({ endpoint, key });
@@ -29,34 +26,65 @@ module.exports = async function (context, req) {
   const container = database.container(containerId);
 
   // req.params is part of the url path
-  const theId = req.params.id;
   // req.query is part of the query string
+  const theId = req.params.id;
   // const theId = req.query.id;
   const theType = req.query.type;
   const newObject = req.body;
 
-  // retrieve the object to be updated from db using the supplied id and partition key
-  const { resource: theDevice } = await container.item(theId, theType).read();
+  try{
 
-  // destructure id and type from the object to update to use when updating the object
-  const { id, type } = theDevice;
+    // retrieve the object to be updated from db using the supplied id and partition key
+    const { resource: theDevice } = await container.item(theId, theType).read();
+    // destructure id and type from the object to update to use when updating the object
+    const { id, type } = theDevice;
+    // assign all the values passed in from the form object to update the original object
+    const updatedDevice = {...theDevice, ...newObject};
 
-  // assigning all the values passed in from the form and updating the original object
-  const updatedDevice = {...theDevice, ...newObject};
+    try{
+      
+      // update the old object by replacing it with updatedDevice object, returns updated_device
+      const { resource: updated_device } = await container
+        .item(id, type)
+        .replace(updatedDevice);
 
-  // update the old object by replacing it with updatedDevice object, returns updated_device
-  const { resource: updated_device } = await container
-    .item(id, type)
-    .replace(updatedDevice);
+      const responseMessage = {
+        status:200,
+        message:"OK",
+        description:"Update successful",
+        device:updated_device
+      }
 
-  const responseMessage = {
-    status: 200,
-    message: "Ok",
-    device: updated_device,
-  };
+      context.res = {
+        body: responseMessage,
+      };
+    
+    }catch(error){
+      
+      const responseMessage = {
+        status:error.code,
+        message:error.message,
+        description:"FAILED on update"
+      }
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
+      context.res = {
+        body: responseMessage,
+      };
+      
+    }
+
+  }catch (error){
+
+    const responseMessage = {
+      status: error.code,
+      message: error.message,
+      description: "FAILED on finding record"
+    }
+
+    context.res = {
+      body: responseMessage,
+    };
+    
+  }
+  
 };
